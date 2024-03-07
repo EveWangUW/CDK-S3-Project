5e35d08e7e9295b471a76d4a5d68d320340c4273
Evewwal12 <wxy1012candoit@163.com>
2024-03-07 14:57:07 -0800

    initial cdk s3 project

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,25 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket, CfnBucket, EventType} from 'aws-cdk-lib/aws-s3'; 
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //L1 and L2 construct of an S3 bucket
+    const level1S3Bucket = new CfnBucket(this,'MyFirstLevelConstructBucket',{
+      versioningConfiguration:{
+        status: "Enabled"
+      }
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const level2S3Bucket = new Bucket(this,'MyFirstLevl2ConstructBucket',{
+      //removalPolicy:cdk.RemovalPolicy.DESTROY,
+      bucketName:"eviesfirstbucket1012",
+      versioned: true
+    });
   }
+
+  //how to link a s3 put event to a sqs queue using cdk and typescript
+  
 }
